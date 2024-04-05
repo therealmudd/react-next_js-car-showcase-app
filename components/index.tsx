b95803e8b073ec7b981be1f89ab0f1c77ddecf51
therealmudd <m.antonio0294@gmail.com>
2024-04-05 18:42:29 +0200

    add files

---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer
+};
